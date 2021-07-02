c7a812525ee7018b83199cad088c7f55bb342d0a
qinxiaowen <qinxiaowen@virtaitech.com>
2021-07-02 16:47:21 +0800

    update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module coinbar
+
+go 1.15
+
+require (
+	github.com/caseymrm/menuet v1.0.1
+	github.com/sirupsen/logrus v1.8.1
+)
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/caseymrm/menuet"
+)
+
+func getPricesSetTitle() {
+	defer time.Sleep(5 * time.Second)
+	url := fmt.Sprintf("%s%s", "https://api1.binance.com", "/api/v3/ticker/price")
+	var prices []SymbolPrice
+	err := getJson(url, &prices)
+	if err != nil {
+		logrus.Error(err)
+	}
+	nearPrice := 0.0
+	btcPrice := 0.0
+	gtcPrice := 0.0
+
+	for _, item := range prices {
+		switch item.Symbol {
+		case "NEARUSDT":
+			nearPrice = item.Price
+			break
+		case "GTCUSDT":
+			gtcPrice = item.Price
+			break
+		case "BTCUSDT":
+			btcPrice = item.Price
+			break
+		}
+	}
+
+	title := fmt.Sprintf("GTC: %v NEAR: %v BTC: %v", gtcPrice, nearPrice, btcPrice)
+	menuet.App().SetMenuState(&menuet.MenuState{
+		Title: title,
+	})
+
+}
+
+func priceLoop() {
+	for {
+		getPricesSetTitle()
+	}
+}
+
+func main() {
+	go priceLoop()
+	menuet.App().RunApplication()
+}
+
+type SymbolPrice struct {
+	Symbol string  `json:"symbol"`
+	Price  float64 `json:"price,string"`
+}
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(body, &target)
+	if err != nil {
+		return err
+	}
+	return nil
+}
